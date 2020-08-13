6d633d550f82bc3991f6eb05dfdb0fa3d36735d8
barisalbyrak <barisalbayrak34@gmail.com>
2020-08-13 17:16:15 +0300

    fully updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,17 @@ $(function () {
       }
   });
 });
+
+$(document).ready(function(){
+    $('.arg0_swiper').slick({
+      autoplay: true,
+      autoplaySpeed: 4000,
+      arrows: false,
+    });
+    $('#arg0_swiper--prev').on('click', function() {
+      $('.arg0_swiper').slick('slickPrev');
+    });
+    $('#arg0_swiper--next').on('click', function() {
+      $('.arg0_swiper').slick('slickNext');
+    });
+  });
