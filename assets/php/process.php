c0f0bfef081407952b1ae8ab9f7956cd3a0ab913
barisalbyrak <barisalbayrak34@gmail.com>
2020-08-18 09:48:44 +0300

    php_mailer(fully updated)

--- assets/php/process.php
+++ assets/php/process.php
@@ -1,30 +1,44 @@
-﻿
-<meta http-equiv="refresh" content="2;URL=/index.html">
+﻿<?php
 
-<?
-require("class.phpmailer.php");
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
 $mail = new PHPMailer();
 
-$mail->IsSMTP();
-$mail->Host     = "srvc184.turhost.com";
-$mail->SMTPAuth = true;
-$mail->Username = "arg0@eryilmazlar.net";
-$mail->Password = "";
-$mail->CharSet = 'UTF-8';
-$mail->Port = 465;
-$mail->From     = "arg0@eryilmazlar.net";
-$mail->Fromname = "giden ismi";
-$mail->AddAddress("arg0@eryilmazlar.net","Websitenizden gelen Siparişiniz.");
-$mail->Subject  =  $_POST['email'];
-$mail->Body     =  implode("  ",$_POST);
-if(!$mail->Send())
-{
-   echo "Mesaj Gönderilemedi <p>";
-   echo "Mailer Error: " . $mail->ErrorInfo;
-   exit;
-}
 
-echo '<script type="text/javascript">alert("E Postanız  Başarı ile  Gönderilmiştir.");</script>';
+$mail->isSMTP();
+$mail->SMTPKeepAlive = true;
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls'; //ssl
+$mail->CharSet = 'UTF-8';
+$mail->Port = 587; //25 , 465 , 587
+$mail->Host = "srvc184.turhost.com";
+
+$mail->Username = "arg0@eryilmazlar.net";
+$mail->Password = "N0P455W0RD";
+
+
+$mail->setFrom("noreply@eryilmazlar.net");
+$mail->addAddress("arg0@eryilmazlar.net");
+
+$body = file_get_contents('./process.html');
+
+$gelen = ["flow_1","flow_2","flow_3","flow_4"];
+$giden = [$_POST['email'],$_POST['phone'],$_POST['subject'],$_POST['address']];
+
+$body = str_replace($gelen,$giden,$body);
+
+$mail->isHTML(true);
+$mail->Subject = "Eryılmazlar Müşteri Destek Hattı";
+$mail->Body = $body;
+
+if ($mail->send())
+    echo '<script>alert("İletinizi teslim aldık. Müşteri destek ekibimiz kısa süre içerisinde talebinizi değerlendirecektir. Teşekkür ederiz.")</script>';
+else
+    echo '<script>alert("İletiniz gönderilemedi. Lütfen daha sonra tekrar deneyiniz.")</script>';
 
 ?>
